5d799558fafe5dd1bc99a5080ebc953c011dbc27
idksus124-dot <idksus124@gmail.com>
2026-06-16 11:08:25 +0530

    Rebrand: Create BlatantDiddyClient.java with new package

---
+++ src/main/java/io/github/idksus124dot/blatantdiddy/client/BlatantDiddyClient.java
@@ -0,0 +1,14 @@
+package io.github.idksus124dot.blatantdiddy.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+/**
+ * BlatantDiddy Client Initializer
+ */
+public final class BlatantDiddyClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        // Client initialization
+    }
+}
